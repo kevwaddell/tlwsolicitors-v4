1d386b650f78eccd495484aa9b5e6809954c5b1b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2015-08-12 11:05:49 +0100

    Wed 12 Aug 2015 - Capitalise gravity forms name input field - fix for full name fields.

--- _/inc/global/forms-script-cap-name.php
+++ _/inc/global/forms-script-cap-name.php
@@ -4,9 +4,13 @@ jQuery.noConflict();
 
 		$('li.capitalize input').keyup(function(event) {
 		    var textBox = event.target;
+		    var txt = $(this).val();
 		    var start = textBox.selectionStart;
 		    var end = textBox.selectionEnd;
-		    textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
+		    //textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
+		    $(this).val(txt.replace(/^(.)|(\s|\-)(.)/g, function($1) {
+            return $1.toUpperCase();
+	        }));
 		    textBox.setSelectionRange(start, end);
 		});
 
