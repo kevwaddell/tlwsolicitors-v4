b2762c8b9c28824c5febfb4111869db5bc8cf836
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2015-09-08 14:31:44 +0100

    Tue 8 Sept 2015 - change services menus to include related pages.

--- _/inc/business-service-page/links-menu.php
+++ _/inc/business-service-page/links-menu.php
@@ -6,6 +6,7 @@
 				
 				<?php foreach ($links as $link) { 
 				$pg = get_page($link['page_link']);
+				$related_pages = get_field('page_links', $link['page_link']);
 				$clr = get_field('page_colour', $link['page_link']);
 				$pg_icon = get_field('page_icon', $link['page_link']);
 				$child_args = array(
@@ -16,9 +17,14 @@
 				?>
 				<li class="col-xs-12 link-col-<?php echo $clr; ?><?php echo (empty($children)) ? '':' with-children closed'; ?>">
 					<a href="<?php echo get_permalink($pg->ID); ?>"<?php echo (empty($children)) ? '':' class="dropdown-link"'; ?>><i class="fa <?php echo $pg_icon; ?> fa-3x"></i><?php echo get_the_title($pg->ID); ?></a>
-					<?php if (!empty($children)) { ?>
+					<?php if (!empty($children) || !empty($related_pages)) { ?>
 						<ul class="list-unstyled children row"> 
 							<li class="col-xs-4"><a href="<?php echo get_permalink($pg->ID); ?>">Overview</a></li>
+							<?php if ($related_pages) { ?>
+								<?php foreach ($related_pages as $rel_pg) { ?>
+								<li class="col-xs-4"><a href="<?php echo get_permalink($rel_pg['page']->ID); ?>"><?php echo $rel_pg['link_title']; ?></a></li>
+								<?php } ?>
+							<?php } ?>
 							<?php foreach ($children as $child) { ?>
 							<li class="col-xs-4"><a href="<?php echo get_permalink($child->ID); ?>"><?php echo get_the_title($child->ID); ?></a></li>
 							<?php } ?>
--- _/inc/service-page/links-menu.php
+++ _/inc/service-page/links-menu.php
@@ -1,4 +1,5 @@
 <?php if (!empty($links)) { ?>
+
 <div class="links-menu closed">
 	<div class="links-menu-inner">
 		<div class="container">
@@ -6,8 +7,11 @@
 				
 				<?php foreach ($links as $link) { 
 				$pg = get_page($link['page_link']);
+				$related_pages = get_field('page_links', $link['page_link']);
 				$clr = get_field('page_colour', $link['page_link']);
 				$pg_icon = get_field('page_icon', $link['page_link']);
+				
+				//Load child pages for parent page
 				$child_args = array(
 				'sort_column' => 'menu_order',
 				'parent'	=> $link['page_link']
@@ -16,9 +20,14 @@
 				?>
 				<li class="col-xs-12 link-col-<?php echo $clr; ?><?php echo (empty($children)) ? '':' with-children closed'; ?>">
 					<a href="<?php echo get_permalink($pg->ID); ?>"<?php echo (empty($children)) ? '':' class="dropdown-link"'; ?>><i class="fa <?php echo $pg_icon; ?> fa-3x"></i><?php echo get_the_title($pg->ID); ?></a>
-					<?php if (!empty($children)) { ?>
+					<?php if (!empty($children) || !empty($related_pages)) { ?>
 						<ul class="list-unstyled children row"> 
 							<li class="col-xs-4"><a href="<?php echo get_permalink($pg->ID); ?>">Overview</a></li>
+							<?php if ($related_pages) { ?>
+								<?php foreach ($related_pages as $rel_pg) { ?>
+								<li class="col-xs-4"><a href="<?php echo get_permalink($rel_pg['page']->ID); ?>"><?php echo $rel_pg['link_title']; ?></a></li>
+								<?php } ?>
+							<?php } ?>
 							<?php foreach ($children as $child) { ?>
 							<li class="col-xs-4"><a href="<?php echo get_permalink($child->ID); ?>"><?php echo get_the_title($child->ID); ?></a></li>
 							<?php } ?>
