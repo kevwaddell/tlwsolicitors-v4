99c529ef06442807ad6e0ff88f61a5e8a132201d
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2015-09-25 14:04:22 +0100

    Update FAQ drop downs answer to have border on last item.

--- _/inc/service-page/faqs.php
+++ _/inc/service-page/faqs.php
@@ -7,13 +7,17 @@ if ($faqs_active) { ?>
 $faqs_id = get_field('faqs');
 $faq_post = get_post($faqs_id);
 $faq_qestions = get_field('faq_qestions',$faq_post->ID);
+$faqs_total = count($faq_qestions);
+$faqs_counter = 0;
 //echo '<pre>';print_r($faq_qestions);echo '</pre>';
 ?>
 <section id="faqs" class="col-<?php echo (!empty($color)) ? $color : 'red'; ?>">
 	<h3 class="icon-header">FAQ's <i class="fa fa-comments fa-lg"></i></h3>
 	
-	<?php foreach ($faq_qestions as $q) { ?>
-	<div class="faq-item item-closed">
+	<?php foreach ($faq_qestions as $q) { 
+	$faqs_counter++;
+	?>
+	<div class="faq-item item-closed<?php echo ($faqs_counter === $faqs_total) ? " last-item" : ''; ?>">
 		<div class="faq-question"><?php echo $q['faq_question']; ?></div>
 		<div class="faq-answer">
 			<?php echo $q['faq_answer']; ?>
