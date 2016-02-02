421250452699c7baf81568c346b9bf0bfeebc32c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-02 11:37:11 +0000

    Add fav icon to theme.

--- _/inc/global/head-html.php
+++ _/inc/global/head-html.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content ="width=device-width,user-scalable=yes" />
 	<meta name="format-detection" content="telephone=yes">
 	
-	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/_/img/favicon.ico">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
 	
