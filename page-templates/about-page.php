3287fbdb0c9c5e16103d9f5c2ff568b6607c7848
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-07-25 15:01:07 +0100

    Added new about us page.

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -1,10 +1,10 @@
 <?php 
 /*
-Template Name: General Service page
+Template Name: About page template
 */
  ?>
-
-<?php get_header(); ?>	
+ 
+ <?php get_header(); ?>	
 	
 	<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
 	<?php 
