d32730f143c35bbbaed04a4186d251d584c1c0fb
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-08-18 10:13:20 +0100

    Update thank you page.

--- page-templates/thank-you-page.php
+++ page-templates/thank-you-page.php
@@ -6,13 +6,9 @@ Template Name: Thank page template
 
 <?php get_header(); ?>
 		
-	<!-- MAIN CONTENT START -->
-	<div class="container">
-	
-		<div class="content">
 
 		<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
-		<main class="<?php echo (!empty($color)) ? ' page-col-'.$color : 'red'; ?>">	
+		<main>	
 		<?php 
 		if (isset($_GET['return-page'])) {
 		$return_id = $_GET['return-page'];
@@ -22,20 +18,26 @@ Template Name: Thank page template
 		?>
 		
 				
-			<article <?php post_class(); ?>>
+			<article <?php post_class("content-section"); ?>>
 				
-				<div class="row">
+				<?php include (STYLESHEETPATH . '/_/inc/sections/global-telephone-number.inc'); ?>		
+	
+				<?php include (STYLESHEETPATH . '/_/inc/global/col-strip.php'); ?>
+				
+				<div class="container">
 					
-					<?php include (STYLESHEETPATH . '/_/inc/global/access-btns.php'); ?>
-					
-					<div class="col-xs-10">
-					
-					<h1>Thank you for contacting us.</h1>
-					<p>A member of our team will contact you shortly.</p>
-					<a href="<?php echo get_permalink($return_id) ; ?>" class="icon-btn" style="padding-left:10px; width: 300px;" id="reload-form">Continue <i class="fa fa-angle-right fa-lg"></i></a>
-											
+					<div class="entry wide-entry">
+						
+						<div class="main-txt">
+							<div class="well well-lg text-center thank-you-message">
+								<h1>Thank you for contacting us.</h1>
+								<p>A member of our team will contact you shortly.</p>
+								<a href="<?php echo get_permalink($return_id) ; ?>" class="btn btn-block icon-btn">Continue</a>
+							</div>
+							
+						</div>
+																	
 					</div>
-					
 				</div>
 				
 			</article>
@@ -44,9 +46,6 @@ Template Name: Thank page template
 		</main>		
 		<?php endwhile; ?>
 		<?php endif; ?>
-
-		</div><!-- CONTENT END -->
 		
-	</div><!-- MAIN CONTENT CONTAINER END -->
-
+		
 <?php get_footer(); ?>
